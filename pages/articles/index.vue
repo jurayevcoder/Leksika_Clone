dfdd293ca99201d4120dfc3df9ad720a7d068509
jurayevcoder <davlatjurayev.1@gmail.com>
2023-05-28 16:06:12 +0500

    articles

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,13 +1,242 @@
 <template>
-    <div>
+  <div class="container mx-auto">
+    <div class="wrapper p-6 flex">
+      <div class="flex gap-10">
+        <div class="flex flex-col gap-5 w-[738px]">
+          <div class="relative">
+            <i class="bx bx-search absolute left-8 top-5 text-[#AAAAAA] text-[25px]"></i>
+            <input
+              type="text"
+              class="w-[738px] h-[64px] rounded-[18px] pl-16 outline-none"
+              placeholder="Search articles here..."
+            />
+          </div>
 
+          <div class="flex gap-5 bg-white rounded-[18px] p-4 w-full">
+            <img src="../../img/book.png" alt="" class="w-[271px] h-[225px]" />
+            <div class="flex flex-col gap-5">
+              <div class="text-[#8C8B8B] flex items-center gap-10">
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-calendar-event"></i>
+                  <p>1 month ago</p>
+                </div>
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-book-open"></i>
+                  <p>number of reads: 52</p>
+                </div>
+              </div>
+              <h1 class="text-[24px] font-bold">
+                Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli
+              </h1>
+              <p>
+                Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish,
+                ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham
+                bu ishni uddalay olmaydi. So’z boyligini oshirish ...
+              </p>
+            </div>
+          </div>
+
+          <div class="flex gap-5 bg-white rounded-[18px] p-4 w-[738px]">
+            <img src="../../img/book.png" alt="" class="w-[271px] h-[225px]" />
+            <div class="flex flex-col gap-5">
+              <div class="text-[#8C8B8B] flex items-center gap-10">
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-calendar-event"></i>
+                  <p>1 month ago</p>
+                </div>
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-book-open"></i>
+                  <p>number of reads: 52</p>
+                </div>
+              </div>
+              <h1 class="text-[24px] font-bold">
+                Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli
+              </h1>
+              <p>
+                Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish,
+                ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham
+                bu ishni uddalay olmaydi. So’z boyligini oshirish ...
+              </p>
+            </div>
+          </div>
+
+          <div class="flex gap-5 bg-white rounded-[18px] p-4 w-[738px]">
+            <img src="../../img/book.png" alt="" class="w-[271px] h-[225px]" />
+            <div class="flex flex-col gap-5">
+              <div class="text-[#8C8B8B] flex items-center gap-10">
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-calendar-event"></i>
+                  <p>1 month ago</p>
+                </div>
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-book-open"></i>
+                  <p>number of reads: 52</p>
+                </div>
+              </div>
+              <h1 class="text-[24px] font-bold">
+                Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli
+              </h1>
+              <p>
+                Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish,
+                ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham
+                bu ishni uddalay olmaydi. So’z boyligini oshirish ...
+              </p>
+            </div>
+          </div>
+
+          <div class="flex gap-5 bg-white rounded-[18px] p-4 w-[738px]">
+            <img src="../../img/book.png" alt="" class="w-[271px] h-[225px]" />
+            <div class="flex flex-col gap-5">
+              <div class="text-[#8C8B8B] flex items-center gap-10">
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-calendar-event"></i>
+                  <p>1 month ago</p>
+                </div>
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-book-open"></i>
+                  <p>number of reads: 52</p>
+                </div>
+              </div>
+              <h1 class="text-[24px] font-bold">
+                Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli
+              </h1>
+              <p>
+                Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish,
+                ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham
+                bu ishni uddalay olmaydi. So’z boyligini oshirish ...
+              </p>
+            </div>
+          </div>
+
+          <div class="flex gap-5 bg-white rounded-[18px] p-4 w-[738px]">
+            <img src="../../img/book.png" alt="" class="w-[271px] h-[225px]" />
+            <div class="flex flex-col gap-5">
+              <div class="text-[#8C8B8B] flex items-center gap-10">
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-calendar-event"></i>
+                  <p>1 month ago</p>
+                </div>
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-book-open"></i>
+                  <p>number of reads: 52</p>
+                </div>
+              </div>
+              <h1 class="text-[24px] font-bold">
+                Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli
+              </h1>
+              <p>
+                Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish,
+                ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham
+                bu ishni uddalay olmaydi. So’z boyligini oshirish ...
+              </p>
+            </div>
+          </div>
+
+          <div class="flex gap-5 bg-white rounded-[18px] p-4 w-[738px]">
+            <img src="../../img/book.png" alt="" class="w-[271px] h-[225px]" />
+            <div class="flex flex-col gap-5">
+              <div class="text-[#8C8B8B] flex items-center gap-10">
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-calendar-event"></i>
+                  <p>1 month ago</p>
+                </div>
+                <div class="flex items-center gap-1 text-[12px]">
+                  <i class="bx bx-book-open"></i>
+                  <p>number of reads: 52</p>
+                </div>
+              </div>
+              <h1 class="text-[24px] font-bold">
+                Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli
+              </h1>
+              <p>
+                Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish,
+                ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham
+                bu ishni uddalay olmaydi. So’z boyligini oshirish ...
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="bg-white rounded-full text-[#8C8B8B] flex items-center p-5 justify-center gap-20"
+          >
+            <button class="hover:text-[#01756C]">
+              <div class="flex items-center gap-1">
+                <i class="bx bx-left-arrow-alt"></i>
+                <p>PREV</p>
+              </div>
+            </button>
+            <p class="bg-[#01756C] px-[18px] py-[10px] rounded-full text-white">1</p>
+            <button class="hover:text-[#01756C]">
+              <div class="flex items-center gap-1">
+                <p>NEXT</p>
+                <i class="bx bx-right-arrow-alt"></i>
+              </div>
+            </button>
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-5 w-[522px]">
+          <div class="mt-7 flex flex-col justify-center gap-5">
+            <h1 class="text-[24px] font-bold">Latest articles</h1>
+            <div
+              class="bg-white rounded-[18px] p-4 text-[#AAAAAA] text-[14px] flex flex-col gap-5 w-full"
+            >
+              <div class="flex items-center gap-3">
+                <i class="bx bxs-circle"></i>
+                <a href="#" class="hover:border-b hover:text-[#01756C] border-[#01756C]"
+                  >Ingliz tilidagi so'zlarni eslab qolishning 11 ta samarali usuli</a
+                >
+              </div>
+              <div class="flex items-center gap-3">
+                <i class="bx bxs-circle"></i>
+                <a href="#" class="hover:border-b hover:text-[#01756C] border-[#01756C]"
+                  >Ingliz tilini samarali o'rganish uchun 7 ta maslahat</a
+                >
+              </div>
+              <div class="flex items-center gap-3">
+                <i class="bx bxs-circle"></i>
+                <a href="#" class="hover:border-b hover:text-[#01756C] border-[#01756C]"
+                  >O‘zbek tilining asosiy imlo qoidalari</a
+                >
+              </div>
+              <div class="flex items-center gap-3">
+                <i class="bx bxs-circle"></i>
+                <a href="#" class="hover:border-b hover:text-[#01756C] border-[#01756C]"
+                  >Upwork'da ish boshlash</a
+                >
+              </div>
+              <div class="flex items-center gap-3">
+                <i class="bx bxs-circle"></i>
+                <a href="#" class="hover:border-b hover:text-[#01756C] border-[#01756C]"
+                  >Most Confused Words in English</a
+                >
+              </div>
+              <div class="flex items-center gap-3">
+                <i class="bx bxs-circle"></i>
+                <a href="#" class="hover:border-b hover:text-[#01756C] border-[#01756C]"
+                  >Most Confused Words in English</a
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col gap-5">
+            <h1 class="text-[24px] font-bold">Tags</h1>
+            <div class="flex gap-5 text-[#AAAAAA] flex-wrap">
+              <p class="bg-white py-4 px-5 w-full rounded-full">English (2)</p>
+              <p class="bg-white py-4 px-5 rounded-full">Freelancer (5)</p>
+              <p class="bg-white py-4 px-5 rounded-full">Vocabulary (5)</p>
+              <p class="bg-white py-4 px-5 rounded-full">Uzbek grammar (2)</p>
+              <p class="bg-white py-4 px-5 rounded-full">English grammar (5)</p>
+              <p class="bg-white py-4 px-5 rounded-full">Imlo (2)</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
